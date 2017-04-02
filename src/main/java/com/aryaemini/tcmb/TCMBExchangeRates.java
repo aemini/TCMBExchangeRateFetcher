e6d7079ace61ab50b8133a3aedd27c0009aa3ec7
Arya Emini <arya@aryaemini.com>
2017-04-02 21:12:11 +0300

    started

---
+++ src/main/java/com/aryaemini/tcmb/TCMBExchangeRates.java
@@ -0,0 +1,21 @@
+package com.aryaemini.tcmb;
+
+import com.aryaemini.tcmb.model.ExchangeRate;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class TCMBExchangeRates {
+
+	public List<ExchangeRate> fetchLast() {
+		List<ExchangeRate> exchangeRates = new ArrayList<ExchangeRate>();
+		return exchangeRates;
+	}
+
+	public List<ExchangeRate> fetchByDate(Date date) {
+		List<ExchangeRate> exchangeRates = new ArrayList<ExchangeRate>();
+		return exchangeRates;
+	}
+
+}
