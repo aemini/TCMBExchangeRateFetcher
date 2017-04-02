8472468092912ac1ecf827784dce96e3f75e7b40
Arya Emini <arya@aryaemini.com>
2017-04-02 22:23:12 +0300

    model completed

--- src/main/java/com/aryaemini/tcmb/model/ExchangeRate.java
+++ src/main/java/com/aryaemini/tcmb/model/ExchangeRate.java
@@ -24,4 +24,100 @@ public class ExchangeRate {
 		this.bulletinNo = bulletinNo;
 	}
 
+	public Date getDate() {
+		return this.date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getCurrencyCode() {
+		return this.currencyCode;
+	}
+
+	public void setCurrencyCode(String currencyCode) {
+		this.currencyCode = currencyCode;
+	}
+
+	public String getCurrencyName() {
+		return this.currencyName;
+	}
+
+	public void setCurrencyName(String currencyName) {
+		this.currencyName = currencyName;
+	}
+
+	public Integer getUnit() {
+		return this.unit;
+	}
+
+	public void setUnit(Integer unit) {
+		this.unit = unit;
+	}
+
+	public Double getForexBuying() {
+		return this.forexBuying;
+	}
+
+	public void setForexBuying(Double forexBuying) {
+		this.forexBuying = forexBuying;
+	}
+
+	public Double getForexSelling() {
+		return this.forexSelling;
+	}
+
+	public void setForexSelling(Double forexSelling) {
+		this.forexSelling = forexSelling;
+	}
+
+	public Double getBanknoteBuying() {
+		return this.banknoteBuying;
+	}
+
+	public void setBanknoteBuying(Double banknoteBuying) {
+		this.banknoteBuying = banknoteBuying;
+	}
+
+	public Double getBanknoteSelling() {
+		return this.banknoteSelling;
+	}
+
+	public void setBanknoteSelling(Double banknoteSelling) {
+		this.banknoteSelling = banknoteSelling;
+	}
+
+	public Double getCrossRateUsd() {
+		return this.crossRateUsd;
+	}
+
+	public void setCrossRateUsd(Double crossRateUsd) {
+		this.crossRateUsd = crossRateUsd;
+	}
+
+	public Double getCrossRateOther() {
+		return this.crossRateOther;
+	}
+
+	public void setCrossRateOther(Double crossRateOther) {
+		this.crossRateOther = crossRateOther;
+	}
+
+	@Override
+	public String toString() {
+		return "ExchangeRate [bulletinNo=" + this.bulletinNo +
+				",date=" + this.date +
+				",currencyCode=" + this.currencyCode +
+				",currencyName=" + this.currencyName +
+				",unit=" + this.unit +
+				",forexBuying=" + this.forexBuying +
+				",forexSelling=" + this.forexSelling +
+				",banknoteBuying=" + this.banknoteBuying +
+				",banknoteSelling=" + this.banknoteSelling +
+				",crossRateUsd=" + this.crossRateUsd +
+				",crossRateOther=" + this.crossRateOther +
+				"]";
+	}
+
 }
